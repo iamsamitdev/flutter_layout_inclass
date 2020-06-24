a15a4c2e09099bd5e1b139c762674aac1b797244
Samit Koyom <samitkoyom@gmail.com>
2020-06-24 23:32:52 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/itgenius/flutter_layout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.itgenius.flutter_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout/container_widget_layout.dart';
+import 'center_widget_layout.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple
+      ),
+      home: ContainerWidgetPage(),
+    );
+  }
+}
+
